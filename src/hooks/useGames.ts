5edb184a88984a6223b7c5fc04f037ba88bdce9c
Petar <petarkuzmanoski96@gmail.com>
2023-10-13 16:27:38 +0100

    Building game cards.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
@@ -21,7 +22,9 @@ const useGames = () => {
 
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
